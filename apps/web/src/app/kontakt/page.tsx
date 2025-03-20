a008f554bde719750d362d693b76c3dabe09b749
01szak <kacperolszewski2006@gmail.com>
2025-03-20 21:03:04 +0100

    fix for mobile

--- apps/web/src/app/kontakt/page.tsx
+++ apps/web/src/app/kontakt/page.tsx
@@ -7,20 +7,16 @@ import Image from "next/image";
 export default function Page() {
   return (
     <div className={"font-jet-brains"}>
-      <h1 className="ml-35 text-6xl font-extrabold text-primary md:text-4xl  md:mb-10">
+      <h1 className="text-4xl  ml-5 mb-5 font-extrabold text-primary md:ml-35 md:text-6xl md:mb-10">
         Kontakt
       </h1>
-      <div
-        className="w-[1270px] h-[714px] grid grid-cols-2 grid-rows-2 gap-[56px] justify-self-center md:w-[750px] md:h-[400px] md:mb-5 md:gap-[30]">
+      <div className=" w-full flex flex-col md:grid md:grid-cols-2 md:grid-rows-2 md:gap-[56px] md:justify-self-center md:w-[750px] md:h-[400px] md:mb-5 md:gap-[30] ">
         <div
-          className={
-            "col-span-2 row-span-1  bg-(--color-primary) large-container "
-          }
-        >
-          <div className={"ml-10 text-(--color-secondary-300) flex flex-row justify-between md:mt-3 md:ml-8"}>
-            <p className={"text-6xl md:text-3xl font-bold   "}>
+          className={"small-container bg-(--color-primary) w-full h-[200px] mb-5 md:col-span-2 row-span-1 md:large-container "}>
+          <div className={"ml-2 text-(--color-secondary-300) mt-2 flex flex-row justify-between  md:ml-8"}>
+            <h1 className={"text-2xl font-black md:text-6xl "}>
               Portale społecznościowe
-            </p>
+            </h1>
             <Image
               src={"bolt.svg"}
               alt="alt"
@@ -31,10 +27,10 @@ export default function Page() {
           </div>
           <div
             className={
-              "ml-10 mt-10 text-(--color-secondary-300) flex flex-col md:mt-2 md:ml-8"
+              "ml-2 md:mt-10 text-(--color-secondary-300) flex flex-col  md:ml-8"
             }
           >
-            <p className={"text-4xl mb-5 md:text-xl"}>
+            <p className={"text-lg text-black my-3  md:text-xl"}>
               Napisz do nas na naszych portalach społecznościowych!
             </p>
             <div className={"flex flex-row  md:mt-5"}>
@@ -46,9 +42,9 @@ export default function Page() {
                 <Image
                   src={"instagram-logo.svg"}
                   alt="Instagram"
-                  width="70"
-                  height="70"
-                  className={"mx-5 md:w-[35] md:h-[35] md:mx-3"}
+                  width="35"
+                  height="35"
+                  className={"mx-5 md:w-[70] md:h-[70] md:mx-3"}
                 />
               </Link>
               <Link
@@ -59,9 +55,9 @@ export default function Page() {
                 <Image
                   src={"facebook-logo.svg"}
                   alt="alt"
-                  width="70"
-                  height="70"
-                  className={"mx-5 md:w-[35] md:h-[35] md:mx-3"}
+                  width="35"
+                  height="35"
+                  className={"mx-5 md:w-[70] md:h-[70] md:mx-3"}
                 />
               </Link>
 
@@ -73,31 +69,21 @@ export default function Page() {
                 <Image
                   src={"linkedin-logo.svg"}
                   alt="Linkedin"
-                  width="70"
-                  height="70"
-                  className={"mx-5 md:w-[35] md:h-[35] md:mx-3"}
+                  width="35"
+                  height="35"
+                  className={"mx-5 md:w-[70] md:h[70] md:mx-3"}
                 />
               </Link>
             </div>
           </div>
         </div>
         <div
-          className={
-            "col-span-1 row-span-1 bg-(--color-secondary-300) small-container "
-          }
-        >
+          className={"w-full h-[200px] bg-(--color-secondary-300) small-container mb-5 md:col-span-1 md:row-span-1 "}>
           <h1
-            className={
-              "ml-10 mt-10 text-primary text-6xl font-bold md:text-3xl md:mt-3  md:ml-8"
-            }
-          >
+            className={"ml-2 mt-2 text-2xl font-black md:text-6xl text-primary  md:text-3xl md:mt-3  md:ml-8"}>
             Kontakt ogólny
           </h1>
-          <p
-            className={
-              "ml-10  text-4xl text-secondary-100  md:text-xl md:mt-1 md:ml-8"
-            }
-          >
+          <p className={"text-lg text-black my-3 ml-2 text-secondary-100 md:text-4xl md:mt-1 md:ml-8"}>
             Jeśli masz pytania ogólne lub problem z rejestracją, napisz na
             adres:
           </p>
@@ -109,27 +95,16 @@ export default function Page() {
               height="22"
               className={"ml-5"}
             />
-            <a className={" mr-10  text-3xl  md:text-xl "}
+            <a className={" mr-10  text-lg  md:text-xl "}
                href={"mailto:kontakt@hackrena.pl"}>kontakt@hackarena.pl</a>
           </div>
         </div>
-        <div
-          className={
-            "col-span-1 row-span-1 bg-(--color-secondary-300) small-container "
-          }
-        >
-          <h1
-            className={
-              "ml-10 mt-10 text-primary text-6xl font-bold md:text-3xl md:mt-3  md:ml-8"
-            }
-          >
+        <div className={"w-full h-[200px] bg-(--color-secondary-300) small-container mb-5 md:col-span-1 md:row-span-1 "}>
+          <h1 className={"ml-2 mt-2 text-2xl font-black md:text-6xl text-primary  md:text-3xl md:mt-3  md:ml-8"}>
             Współpraca
           </h1>
           <p
-            className={
-              "ml-10  text-4xl text-secondary-100  md:text-xl md:mt-1 md:ml-8"
-            }
-          >
+            className={"text-lg text-black my-3 ml-2 text-secondary-100 md:text-4xl md:mt-1 md:ml-8"}>
             Jeśli jesteś zainteresowany współpracą z nami, napisz na adres:
           </p>
           <div className={'flex flex-row justify-around'}>
@@ -140,7 +115,7 @@ export default function Page() {
               height="22"
               className={"ml-6"}
             />
-            <a className={"  text-3xl  md:text-xl "}
+            <a className={" mr-10  text-lg  md:text-xl "}
                href={"mailto:wspolpraca@hackrena.pl"}>wspolpraca@hackarena.pl</a>
           </div>
         </div>
