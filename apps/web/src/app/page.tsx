c72d2e9c5dfddc4958b5ebb557db402ff34e05cd
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-03-21 15:43:25 +0100

    feat(apps/web): add home page layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,7 +1,63 @@
+import { LinkButton } from "@repo/ui";
+import Image from "next/image";
+import Link from "next/link";
+import { ComponentProps } from "react";
+
+const Section = ({ children, ...props }: ComponentProps<"section">) => (
+  <section className={`w-full flex flex-col items-center px-4  ${props.className}`}>{children}</section>
+);
+
 export default function Home() {
   return (
-    <div className="w-full flex justify-center">
-      <h1 className="text-4xl russo-one text-primary">Home page</h1>
+    <div className="w-full flex flex-col overflow-hidden">
+      <Section className="relative py-40">
+        <Image src="/home-bg.svg" width={100} height={100} className="absolute top-[40%] right-0 -translate-y-1/2 w-[70%] max-h-[700px] pointer-events-none" alt="Tło" />
+        <div className="w-full flex flex-col items-start page-width z-2">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-5xl russo-one font-bold">
+              Hack
+              <Image src="lightning.svg" className="inline" width={30} height={30} alt="⚡" />
+              Arena
+            </h1>
+            <p className="text-2xl">Zbierz zespół i zdobywaj nagrody!</p>
+          </div>
+          <div className="flex flex-row gap-6 mt-22">
+            <LinkButton href="/wydarzenia">Wydarzenia</LinkButton>
+            <LinkButton href="/o-nas" secondary icon>O nas</LinkButton>
+          </div>
+        </div>
+      </Section>
+      <Section className="py-0 md:py-40 relative">
+        <Image src="/arrows-right-bg.svg" width={100} height={100} className="absolute top-[10%] md:top-[40%] -left-[30%] md:left-0 -translate-y-1/2 w-[90%] md:w-[40%] min-w-[450px] md:min-w-[600px] max-h-[250px] pointer-events-none" alt="Tło" />
+        <div className="w-full flex flex-col items-start page-width z-2 gap-4">
+          <h2 className="text-5xl text-primary font-bold russo-one">
+            O nas
+          </h2>
+          <p className="text-secondary-100 text-xl max-w-[900px]">HackArena to hackathon organizowany przez <strong className="text-white">Koło Naukowe init z SGGW w Warszawie.</strong> Podczas wydarzenia zespoły programistów tworzą boty do gry komputerowej, które następnie walczą przeciwko sobie w <strong className="text-white">turnieju</strong> w celu uzyskania tytułu <strong className="text-white">mistrza HackAreny.</strong></p>
+        </div>
+      </Section>
+      <Section className="py-40 relative">
+        <Image src="/arrows-left-bg.svg" width={100} height={100} className="absolute top-0 md:-translate-y-1/2 translate-y-1/2 translate-x-1/2 md:translate-x-0 right-0 w-full md:w-[50%] min-w-[700px] max-h-[250px] pointer-events-none" alt="Tło" />
+        <div className="w-full flex flex-col items-start page-width z-2 gap-8">
+          <h2 className="text-5xl text-primary font-bold russo-one">
+            Ostatnie
+          </h2>
+          <div className="grid grid-rows-auto md:grid-cols-[.4fr_1fr] gap-6">
+            <div className="p-2 ribbon relative min-w-[250px]">
+              <Image src="/hackarena-2-thumb.jpg" className="ribbon-photo-clip w-full" width={400} height={200} alt="HackArena 2" />
+              <div className="absolute inset-0 flex flex-col w-full justify-center items-center bg-background mix-blend-color" />
+            </div>
+            <div className="flex flex-col justify-around ">
+              <h4 className="text-primary text-2xl">HackArena 2.0</h4>
+              <p className="text-secondary-100 text-xl">HackArena 2.0 to druga edycja hackathonu organizowanego przez Koło Naukowe "init" z SGGW...</p>
+              <Link href="/wydarzenia/hackarena-2">
+                <Image src="/arrow-pointer.svg" className="inline" width={30} height={30} alt="Czytaj więcej" />
+                &nbsp;Dowiedz się więcej
+              </Link>
+            </div>
+          </div>
+        </div>
+      </Section>
     </div>
   );
 }
