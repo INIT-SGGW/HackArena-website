03c8b225beefd4011b3730285a174d9e39836a50
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-03-25 18:42:18 +0100

    feat (packages/ui): create Input component

--- apps/web/src/app/konto/page.tsx
+++ apps/web/src/app/konto/page.tsx
@@ -1,68 +1,32 @@
-import { Page } from '@repo/ui';
+import { Button, Input, Page } from '@repo/ui';
 import Image from 'next/image';
 
 export default function LoginForm() {
   return (
     <Page>
-      <h1 className={'justify-self-center text-primary mb-10 text-5xl'}>
+      <h1 className={'russo-one justify-self-center text-primary mb-10 text-5xl'}>
         Logowanie
       </h1>
-      <div
+      <form
         className={
-          'content-card-clip bg-secondary-300 flex flex-col w-full max-w-[410px] justify-self-center  '
+          'content-card-clip bg-secondary-300 flex flex-col w-full max-w-[410px] justify-self-center p-5 gap-1'
         }
-        style={{ '--clip-size': '25px' }}
       >
-        <div className={'p-5'}>
-          <div className={'p-1'}>Email</div>
-          <div
-            className={
-              'content-card-clip  bg-text text-secondary-300 min-h-[35px]'
-            }
-            style={{ '--clip-size': '14px' }}
-          >
-            <input
-              className={'pl-4 pt-1 text-start  outline-none w-full '}
-              type={'text'}
-              placeholder={'Email'}
-            />
-          </div>
-
-          <div className={'p-1'}>Hasło</div>
-          <div
-            className={
-              'content-card-clip bg-text text-secondary-300 min-h-[35px] '
-            }
-            style={{ '--clip-size': '14px' }}
-          >
-            <input
-              className={'pl-4 pt-1 text-start outline-none w-full '}
-              type={'password'}
-              placeholder={'Hasło'}
-            />
-          </div>
-
-          <div className={'p-1 text-secondary-300'}>żeby było równo :)</div>
-          <div
-            className={
-              'button-clip bg-primary text-secondary-300 text-black flex items-center justify-center min-h-[35px]'
-            }
-          >
-            <button className={'cursor-pointer '}>Zaloguj się</button>
-          </div>
-          <div className={'flex flex-row p-3 justify-self-center'}>
-            <Image
-              src={'two-arrows.svg'}
-              alt={'two arrows'}
-              width={30}
-              height={30}
-              className={'mr-4'}
-            />
-            {/* missing attachment  */}
-            <a href={'XYZ'}>Zapomniałeś hasła?</a>
-          </div>
+        <Input placeholder='Email' label="Email" />
+        <Input type="password" placeholder='Hasło' label="Hasło" />
+        <div className={'p-1 text-secondary-300'}>żeby było równo :)</div>
+        <Button type="submit" fullWidth>Zaloguj się</Button>
+        <div className={'flex flex-row w-full justify-center p-3 '}>
+          <Image
+            src={'two-arrows.svg'}
+            alt={'two arrows'}
+            width={30}
+            height={30}
+            className={'mr-4'}
+          />
+          <a href={'/password/forgot'}>Zapomniałeś hasła?</a>
         </div>
-      </div>
+      </form>
     </Page>
   );
 }
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,3 +2,4 @@ export { Layout } from './components/layout';
 export { Button } from './components/button';
 export { LinkButton } from './components/linkButton';
 export { Page } from './components/page';
+export { Input } from './components/input';
