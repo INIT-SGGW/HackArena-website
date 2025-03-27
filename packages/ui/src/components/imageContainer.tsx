19b20c381c1dd59411d41076da8e41666c0c9a5e
01szak <kacperolszewski2006@gmail.com>
2025-03-27 01:08:22 +0100

    code refactor in wydarzenia/page.tsx and ImageContainer.tsx

--- packages/ui/src/components/imageContainer.tsx
+++ packages/ui/src/components/imageContainer.tsx
@@ -12,30 +12,26 @@ type ImageContainerProps = ComponentProps<'div'> & {
 export const ImageContainer = ({
   src,
   alt,
-  width = 400,
-  height = 200,
+  width,
+  height,
   color,
   isIcon = false,
   ...props
 }: ImageContainerProps) => {
   return (
-    <div
-      className={`p-2 ribbon ribbon-${color} w-full min-h-[200] h-[${height}] relative z-30 ${isIcon ? 'w-[300px] h-[200px]' : ''}`}
-    >
-      <div
-        className={`w-full h-full flex h-[${height}] min-h-[200] justify-center  `}
-      >
+    <div className={`p-2 ribbon ribbon-${color} w-full min-w-[300px] min-h-[200]   relative z-10 `}>
+      <div className={`w-full h-full flex  min-h-[200] justify-center  `}>
         <Image
           src={src}
           alt={alt}
-          className={`ribbon-photo-clip  ${props.className} ${isIcon ? 'z-10 ' : ''}`}
+          className={`${isIcon ? "z-20 " : "ribbon-photo-clip  "}  ${props.className} `}
           width={width}
           height={height}
         />
+        {isIcon ? (<div className={`w-full h-full  max-w-[290px] max-h-[205px] bg-secondary-500  self-center  absolute ribbon-photo-clip z-0`}></div>) : null}
       </div>
-      <div
-        className={`ribbon-photo-clip absolute inset-0 flex flex-col  justify-center items-center ${!isIcon ? 'bg-background  mix-blend-color w-full min-h-[200] h-[${height}]' : `bg-secondary-500 scale-97  w-[300px] h-[200px]`}`}
-      />
+      <div className={`ribbon-photo-clip absolute inset-0 flex flex-col  justify-center items-center 
+      ${!isIcon ? `bg-background  mix-blend-color w-full min-h-[200] ` : null}`} />
     </div>
-  );
+    );
 };
