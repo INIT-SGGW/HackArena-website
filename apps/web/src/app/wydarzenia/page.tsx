b9dd3e9d7adaa77102847414d5ff7fae31be9776
01szak <kacperolszewski2006@gmail.com>
2025-03-31 23:40:01 +0200

    added animation on scrolling (not yet working properly)

--- apps/web/src/app/wydarzenia/page.tsx
+++ apps/web/src/app/wydarzenia/page.tsx
@@ -1,6 +1,7 @@
+"use client"
 import Image from "next/image";
 import { ImageContainer, Page } from "@repo/ui";
-import { ComponentProps } from "react";
+import { ComponentProps, useEffect } from "react";
 
 type ContainerNameProps = ComponentProps<"div"> & {
   name: string;
@@ -11,7 +12,7 @@ type ContainerNameProps = ComponentProps<"div"> & {
 function ContainerName({ name,isContainer = false,color, ...props }: ContainerNameProps) {
   return (
       <div className={`w-full flex flex-row ${isContainer ? "justify-between" : "justify-center"}   pb-2 text-${color}  ${props.className}`}>
-      <h1 className={"russo-one font-bold text-4xl md:mb-5 md:mb-0 "}>
+      <h1 className={"russo-one font-bold text-4xl  md:mb-0 "}>
         {name}
       </h1>
       {isContainer ?<Image src={"bolt.svg"} alt={"bolt"} width={15} height={15} className={"md:w-7 md:h-7"}/> : null}
@@ -39,16 +40,15 @@ function Forwarding({ tittle, href, isContainer = true ,...props}: ForwardingPro
 const Current = ({...props }: ComponentProps<"div">) => (
   <div className={`text-secondary-300  justify-items-center russo-one ${props.className}`}>
 
-    <div
-      className={" content-card-clip bg-primary flex-grow  w-full max-w-[500px] md:max-w-[900px] px-6 py-4 pb-6  " } style={{ "--clip-size": "20px" }}>
+    <div className={" content-card-clip bg-primary flex-grow  w-full max-w-[500px] md:max-w-[900px] px-6 py-4 pb-6  " } style={{ "--clip-size": "20px" }}>
       <ContainerName name={"W trakcie"} isContainer={true} color={"bg"} />
       <div className={"flex flex-col md:flex-row "}>
 
-        <ImageContainer src={"/hackarena-2-thumb.jpg"} alt={"HackArena 2.0"} color={"secondary-300"} width={800} height={400} />
+        <ImageContainer src={"/hackarena-2-thumb.jpg"} alt={"HackArena 2.0"} color={"secondary-300"} width={550} height={400} />
 
         <div className={"flex flex-col md:pl-10  md:pt-2 gap-8"}>
 
-          <h1 className={" pt-8 md:pt-0 text-4xl"}>
+          <h1 className={" pt-5 md:pt-0pnpm text-4xl"}>
             HackArena 2.5
           </h1>
           <p className={"font-medium text-xl "}>
@@ -65,27 +65,27 @@ const Current = ({...props }: ComponentProps<"div">) => (
 const BottomImages = ({ ...props }: ComponentProps<"div">) => (
   <div className={`flex flex-col md:flex-row shrink justify-center md:gap-20 gap-10  ${props.className}`}>
 
-    <div className={`  self-center   ${props.className} `}>
+    <div className={`self-center ${props.className}`}>
       <ContainerName name={"Nadchodzące"} color={"primary"} className={"scale-70"}/>
 
       <ImageContainer src={"question-mark.svg"} alt={"hackarena 2.5"} width={50} height={50} color={"secondary-400"}  isIcon={true}/>
       <Forwarding tittle={"???"} href={"/wydarzenia/???"} isContainer={false}  className={"pt-3"} />
     </div>
 
-    <div className={"md:pl-20  md:border-solid md:border-l-2 md:border-secondary-300 min-w-0  "}>
+    <div className={"md:pl-20  md:border-solid md:border-l-2 md:border-secondary-300 min-w-0  scroll-container "}>
       <ContainerName name={"Zakończone"} color={"primary"} className={"scale-70"}/>
-      <div className={"flex flex-row overflow-x-scroll snap-x snap-mandatory no-scrollbar w-full md:max-w-[450px]   "}>
+      <div className={"flex flex-row overflow-x-scroll snap-x snap-mandatory no-scrollbar w-full md:max-w-[450px]    "}>
 
-        <div className={" mr-5  snap-center snap-always shrink-0"}>
+        <div className={" ml-5  snap-center snap-always shrink-0 animated self-center"}>
           <ImageContainer src={"/hackarena-2-thumb.jpg"} alt={"HackArena 2.0"} width={280} height={200}/>
           <Forwarding tittle={"HackArena 2.0"} href={"/wydarzenia/hackArena_2.0"} isContainer={false} className={"pt-3"}/>
         </div>
-        <div className={" mr-5 x snap-center snap-always shrink-0 "}>
+        <div className={" ml-5 snap-center snap-always shrink-0 animated "}>
           <ImageContainer src={"/hackarena-2-thumb.jpg"} alt={"HackArena 2.0"} width={280} height={200}/>
           <Forwarding tittle={"HackArena 2.0"} href={"/wydarzenia/hackArena_2.0"} isContainer={false} className={"pt-3"}/>
         </div>
 
-        <div className={" mr-5  snap-center snap-always shrink-0"}>
+        <div className={" ml-5  snap-center snap-always shrink-0 animated "}>
           <ImageContainer src={"/hackarena-2-thumb.jpg"} alt={"HackArena 2.0"} width={280} height={200}/>
           <Forwarding tittle={"HackArena 1.0"} href={"/wydarzenia/hackArena_1.0"} isContainer={false} className={"pt-3"} />
         </div>
@@ -96,9 +96,29 @@ const BottomImages = ({ ...props }: ComponentProps<"div">) => (
 );
 
 export default function Events() {
+
+
+  useEffect(() =>{
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if(entry.isIntersecting) {
+            entry.target.classList.add("show")
+          } else {
+            entry.target.classList.remove("show")
+          }
+        })
+      },
+      {root: document.querySelector(".scroll-container"),threshold: 0.5}
+    );
+    document.querySelectorAll(".animated").forEach((el) => observer.observe(el));
+
+    return () => observer.disconnect();
+  },[]);
+
   return (
-    <Page className={"flex flex-col gap-10 max-w-[1000px] mx-auto  "}>
-      <h1 className={"text-5xl text-primary russo-one pl-4  self-center pr-30 md:self-start md:pl-15 md:pr-0 "}>Wydarzenia</h1>
+    <Page className={"flex flex-col gap-10 max-w-[1000px] mx-auto "}>
+      <h1 className={"text-5xl text-primary russo-one pl-4  self-center pr-30  md:pl-15 md:pr-0 "}>Wydarzenia</h1>
 
       <div className={"flex flex-col md:gap-8 gap-10 justify-self-center min-w-0"}>
         <Current className={" "}></Current>
