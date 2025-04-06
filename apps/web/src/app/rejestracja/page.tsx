178d682d3ec203aacf6076a1ddc0945db28bc8cd
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-04-06 12:09:03 +0200

    feat (apps/web): add Rejestracja page

---
+++ apps/web/src/app/rejestracja/page.tsx
@@ -0,0 +1,18 @@
+import { Page, SocialLinks } from '@repo/ui';
+
+export default function Rejestracja() {
+  return (
+    <Page className="flex flex-col items-center" paddingTop="7rem">
+      <div className="page-width !max-w-[700px] flex flex-col items-center gap-4">
+        <h1 className="text-4xl text-primary font-bold russo-one">
+          Rejestracja
+        </h1>
+        <p className="text-center text-lg">
+          Aktualnie nie prowadzimy zapisów. Śledź nasze portale społecznościowe,
+          żeby nie przegapić następnego wydarzenia!
+        </p>
+        <SocialLinks color={'#fff'} size={30} className="w-min pt-7" />
+      </div>
+    </Page>
+  );
+}
