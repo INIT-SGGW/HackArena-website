b8bbcbc127befff91d11853017a7285db365eb71
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-04-14 18:18:55 +0200

    feat (apps/web): add JWT authentication middleware for protected routes

---
+++ apps/web/src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { jwtVerify } from 'jose'
+
+export async function middleware(req: NextRequest) {
+    const token = req.cookies.get('jwt')?.value
+    const jwtSecret = process.env.JWT_SECRET
+
+    if (!jwtSecret) {
+        throw new Error('JWT_SECRET is not defined')
+    }
+
+    if (!token) {
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+
+    if (token) {
+        try {
+            await jwtVerify(token, new TextEncoder().encode(jwtSecret))
+        } catch (e) {
+            return NextResponse.redirect(new URL('/login', req.url))
+        }
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ['/konto'],
+}
