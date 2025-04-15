cecf2edfa7134987333062cc2b9a92c7aaf56e1d
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-04-15 12:14:30 +0200

    feat (apps/web): move RejestracjaDrużyny page to "rejestracja/druzyna

--- apps/web/src/app/rejestracja/druzyna/page.tsx
+++ apps/web/src/app/rejestracja/druzyna/page.tsx
@@ -1,5 +1,6 @@
 import { Page, SocialLinks } from '@repo/ui';
 import { Metadata } from 'next';
+import { RegisterForm } from '../../../views/Account/RegisterForm';
 
 const metadata: Metadata = {
   title: 'Rejestracja | HackArena',
@@ -10,19 +11,18 @@ const metadata: Metadata = {
   },
 };
 
-export default function Rejestracja() {
+export default function RejestracjaDrużyny() {
   return (
-    <Page className="flex flex-col items-center" paddingTop="7rem">
-      <div className="page-width !max-w-[700px] flex flex-col items-center gap-4">
-        <h1 className="text-4xl text-primary font-bold russo-one">
-          Rejestracja
-        </h1>
-        <p className="text-center text-lg">
+    <Page className="flex flex-col items-center gap-4">
+      <h1 className="text-5xl text-primary font-bold russo-one">
+        Rejestracja
+      </h1>
+      <RegisterForm />
+      {/* <p className="text-center text-lg">
           Aktualnie nie prowadzimy zapisów. Śledź nasze portale społecznościowe,
           żeby nie przegapić następnego wydarzenia!
         </p>
-        <SocialLinks color={'#fff'} size={30} className="w-min pt-7" />
-      </div>
+        <SocialLinks color={'#fff'} size={30} className="w-min pt-7" /> */}
     </Page>
   );
 }
