cf52472860199cb8e7f97b94c2d763b9e430ad16
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-04-18 10:04:54 +0200

    feat (paackages/ui): add Select component and SuccessPage component; enhance Input component with checkbox support

---
+++ packages/ui/src/components/select.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps } from 'react';
+
+type Props = ComponentProps<'select'> & {
+    label?: string;
+    error?: string[];
+    options: string[];
+};
+
+export function Select({ id, label, error, options, ...props }: Props) {
+    return (
+        <div className="flex flex-col gap-1">
+            <label htmlFor={id}>{label}</label>
+            <select
+                {...props}
+                id={id}
+                className={
+                    `bg-text text-background font-bold text-start px-4 py-2.5 sm:py-2 w-full both-corners-clip`
+                }
+            >
+                {
+                    options.map((value, index) => {
+                        return <option key={index} value={value}>{value}</option>
+                    })
+                }
+            </select>
+            {error && <span className="text-sm text-error">{error[0]}</span>}
+        </div >
+    );
+}
---
+++ packages/ui/src/components/successPage.tsx
@@ -0,0 +1,22 @@
+import { Page } from "./page";
+import { SocialLinks } from "./socialLinks";
+
+type Props = {
+    title: string,
+    text: string
+}
+
+export function SuccessPage({ title, text }: Props) {
+    return (
+        <Page
+            className="flex flex-col items-center justify-center gap-8 text-center min-h-[600px]"
+            paddingTop="0px"
+        >
+            <div className="flex flex-col gap-4 items-center">
+                <h1 className="title text-center">{title}</h1>
+                <p className="text-center">{text}</p>
+                <SocialLinks className="mt-8 w-max" color="#fff" size={30} />
+            </div>
+        </Page>
+    )
+}
