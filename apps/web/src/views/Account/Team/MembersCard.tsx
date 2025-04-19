c1082c30a9ec1639e5191c78f94b76c0f27aaf5a
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-04-19 17:40:42 +0200

    feat (apps/web): add crown and leave SVG icons; refactor MembersCard to use new options buttons for member actions

--- apps/web/src/views/Account/Team/MembersCard.tsx
+++ apps/web/src/views/Account/Team/MembersCard.tsx
@@ -1,23 +1,29 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { getUserId } from "../../../utils/getUserId";
+import { useGetUserId } from "../../../utils/useGetUserId";
 import { LinkButton } from "@repo/ui";
+import Image from "next/image";
+import { ComponentProps } from "react";
 
 const mockData = [
     {
-        id: "680203caf6a04ff8c8d34b9f",
+        id: "6802ba98bbb26f540ad4121b",
         name: 'Jan Kowalski',
         email: 'jan.kowalski@sadf.asdf',
         verified: true,
         captain: true,
     },
     {
+        id: "680203caf6a04ff8c8d34b9",
+
         name: 'Karolina Nowak',
         email: 'karolina.nowak@asdf.asdf',
         verified: true,
     },
     {
+        id: "680203caf6a04ff8c8d34b",
+
         email: 'karolina.nowak@asdf.asdf',
         verified: false,
     },
@@ -26,42 +32,83 @@ const mockData = [
 export function MembersCard() {
     const { _id } = useParams();
 
-    const userId = getUserId();
+    const userId = useGetUserId();
     const captain = mockData.find((member) => member.id === userId);
+
+    const handleChangeLeader = (id: string) => {
+        console.log(`Change leader to ${id}`);
+    }
+
+    const handleDeleteMember = (id: string) => {
+        console.log(`Delete member with id ${id}`);
+    }
 
     return (
         <div className="flex flex-col gap-10">
-            <ol className="w-full mx-auto">
+            <ol className="w-full flex flex-col gap-5 mx-auto">
                 {mockData.map((member, index) => (
                     <li
                         key={index}
-                        className="flex flex-col gap-2 mb-5 last:mb-0"
+                        className="flex flex-col sm:flex-row gap-2 justify-between"
                     >
-                        <div className="flex gap-5 justify-between">
-                            <div className="flex flex-col gap-1">
-                                <span className={`text-lg font-semibold ${member.captain ? "text-blue-500" : ""}`}>
-                                    {member.name}
-                                </span>
-                                <span className="text-sm text-gray-500">
-                                    {member.email}
-                                </span>
-                            </div>
-                            <div className="flex gap-3">
-                                {member.verified ? (
-                                    <span className="text-green-500">
-                                        Zweryfikowany
+                        <div className="flex flex-col gap-1">
+                            {
+                                member.verified ? (
+                                    <span className={`text-lg font-semibold ${member.captain ? "text-blue-500" : ""}`}>
+                                        {member.name}
                                     </span>
                                 ) : (
-                                    <span className="text-error">
-                                        Niezweryfikowany
+                                    <span className="text-lg font-semibold text-error">
+                                        Niezweryfikowany/a
                                     </span>
-                                )}
-                            </div>
+                                )
+                            }
+                            <span className="text-sm text-gray-500">
+                                {member.email}
+                            </span>
+                        </div>
+                        <div className="flex gap-3 items-center">
+                            {
+                                captain?.id === userId && !member.captain && (
+                                    <>
+                                        {
+                                            member.verified && (
+                                                <OptionsButtons
+                                                    title="Zmień na lidera"
+                                                    src="/crown-black.svg"
+                                                    className="bg-primary"
+                                                    onClick={() => handleChangeLeader(member.id)}
+                                                />
+                                            )
+                                        }
+                                        <OptionsButtons
+                                            title="Usuń członka"
+                                            src="/leave.svg"
+                                            className="bg-error"
+                                            onClick={() => handleDeleteMember(member.id)}
+                                        />
+                                    </>
+                                )
+                            }
                         </div>
                     </li>
                 ))}
             </ol>
             {captain?.id === userId && <LinkButton href={window.location.href + "/dodaj"} fullWidth>Dodaj członka</LinkButton>}
         </div>
+    )
+}
+
+type OptionsButtonsProps = ComponentProps<"button"> & {
+    title: string;
+    src: string;
+    onClick: () => void;
+}
+
+const OptionsButtons = ({ title, src, onClick, ...props }: OptionsButtonsProps) => {
+    return (
+        <button {...props} onClick={onClick} className={`flex flex-1 sm:flex-auto items-center justify-center w-12 sm:w-10 h-10 rounded-md cursor-pointer ${props.className}`} title={title}>
+            <Image title={title} alt={title} src={src} width={24} height={24} className="w-7" />
+        </button>
     )
 }
