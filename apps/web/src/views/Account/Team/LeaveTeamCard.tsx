ad08be8b45aaabcd3bfd33007af63d150ae04cac
VEXI19 <87818072+VEXI19@users.noreply.github.com>
2025-04-21 17:34:49 +0200

    feat (apps/web): implement leave team functionality in LeaveTeamCard component

--- apps/web/src/views/Account/Team/LeaveTeamCard.tsx
+++ apps/web/src/views/Account/Team/LeaveTeamCard.tsx
@@ -3,17 +3,28 @@
 import { useParams } from "next/navigation";
 import { useGetUserId } from "../../../utils/useGetUserId";
 import { Button } from "@repo/ui";
+import { fetcherHack } from "../../../api/fetcher";
+import useSWRMutation from "swr/mutation";
 
 export function LeaveTeamCard() {
-    const { _id } = useParams();
+    const { id } = useParams();
     const userId = useGetUserId();
 
-    const handleLeaveTeam = () => {
-        // Implement the logic to leave the team
-        console.log("Leaving team with ID:", _id, userId);
+    const { error, isMutating, trigger } = useSWRMutation<null>(`/teams/${id}/members/${userId}/leave-team`, (url: string) => fetcherHack<null, null>(url, {
+        authRedirect: true,
+        method: 'PATCH',
+    }));
+
+    const handleLeaveTeam = async () => {
+        await trigger();
+
+        window.location.href = '/konto';
     }
 
     return (
-        <Button onClick={() => handleLeaveTeam} fullWidth>Opuść drużynę</Button>
+        <div className="flex flex-col gap-1 items-center">
+            <Button onClick={handleLeaveTeam} disabled={isMutating} fullWidth>{isMutating ? "..." : "Opuść drużynę"}</Button>
+            {error && <p className="text-error text-center">Wystąpił problem podczas opuszczania drużyny</p>}
+        </div>
     )
 }
